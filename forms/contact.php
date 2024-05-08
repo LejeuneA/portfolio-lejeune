166b93df5fab372f739f73ec557b728a3c9fe2f0
A.Lejeune <acelyalejeune@gmail.com>
2024-05-08 12:52:26 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -5,16 +5,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = htmlspecialchars($_POST['firstName']);
     $lastName = htmlspecialchars($_POST['lastName']);
     $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
     $userEmail = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
 
     // Check if all fields are filled
-    if (!empty($firstName) && !empty($lastName) && !empty($subject) && !empty($message)) {
+    if (!empty($firstName) && !empty($lastName) && !empty($subject) && !empty($userEmail) && !empty($message)) {
         // Set sender email (you should add an email field to your form)
         $senderEmail = $userEmail;
         
         // Set recipient email
-        $recipient = "acelyalejeune@gmail.com";
+        $recipient = "contact@acelyalejeune.be";
         
         // Construct mail headers
         $mailHeaders = "From: " . $senderEmail . "\r\n";
