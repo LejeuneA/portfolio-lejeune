15faa2b3f5f7825634328cb4e606a17e0bf8365c
A.Lejeune <acelyalejeune@gmail.com>
2026-07-17 15:19:49 +0200

    Create restaurant-pistache-db.php

---
+++ public/restaurant-pistache-db.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'host' => 'mysql-acelyalejeune.alwaysdata.net',
+    'user' => 'acelyalejeune',
+    'password' => '@NtLYa130580',
+    'database' => 'acelyalejeune_restaurant_pistache',
+];
