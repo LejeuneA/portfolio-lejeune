c857d999e4d6743b0b19186c95c303a0a5c02caf
A.Lejeune <acelyalejeune@gmail.com>
2026-07-17 17:25:43 +0200

    user demo active

--- public/restaurant-pistache-db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'host' => 'mysql-acelyalejeune.alwaysdata.net',
-    'user' => 'acelyalejeune',
-    'password' => '@NtLYa130580',
-    'database' => 'acelyalejeune_restaurant_pistache',
-];
